a0807b484acd5a7b91a588f263e4a1b2818a3315
Sisiwenwen500 <151725452+Sisiwenwen500@users.noreply.github.com>
2024-07-21 01:17:51 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '0c423e80-fe3c-426e-9d91-a7f3549f40ef';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
@@ -197,13 +197,6 @@ export default {
                                 'Content-Type': 'text/plain',
                             }
                         });
-
-                    default:
-                        // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.speedtest.net';
-                        url.protocol = 'https:';
-                        request = new Request(url, request);
-                        return await fetch(request);
                 }
             } else {
                 return await vlessOverWSHandler(request);
